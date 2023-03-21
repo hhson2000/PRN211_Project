e98182909ee249557acb7b5be82b958dff68f302
HoangHaiSon <frchoanghaison@gmail.com>
2023-03-21 17:03:45 +0700

    first commit

---
+++ ProjectPrn211/ProjectPrn211/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectPrn211.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
